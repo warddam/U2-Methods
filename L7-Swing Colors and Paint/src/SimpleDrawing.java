92da588a9bdec4cd63ebdb0b12d8b4cd2bca855e
warddam001 <warddam001@warren.k12.in.us>
2021-10-22 13:20:33 -0400

    colors

--- L7-Swing Colors and Paint/src/SimpleDrawing.java
+++ L7-Swing Colors and Paint/src/SimpleDrawing.java
@@ -1,6 +1,6 @@
 public class SimpleDrawing {
 
     public static void main(String[] args) {
-
+new SimpleDrawingGUI();
     }
 }
--- L7-Swing Colors and Paint/src/SimpleDrawingGUI.java
+++ L7-Swing Colors and Paint/src/SimpleDrawingGUI.java
@@ -12,19 +12,37 @@ public class SimpleDrawingGUI {
         frame.setLocationRelativeTo(null);
 
         //ADD THE PANEL TO THE FRAME
-
-
+        JPanel panel = new DrawingPanel();
+        frame.add(panel);
         frame.setVisible(true);
     }
 
-    private class DrawingPanel extends JPanel{
+    private class DrawingPanel extends JPanel {
 
         //Constructor for the class
-        public DrawingPanel(){
+        public DrawingPanel() {
             //set the Background color
+            setBackground(Color.BLUE);
 
         }
 
+        public void paintComponent(Graphics g) {
 
+            super.paintComponent(g);
+            g.fillRect(50, 50, 100, 100);
+            g.setColor(Color.RED);
+
+
+            g.drawRect(150, 50, 100, 200);
+            g.setColor(Color.WHITE);
+
+            g.setColor(Color.green);
+            g.drawLine(0,0,400,600);
+
+
+
+
+
+        }
     }
 }
