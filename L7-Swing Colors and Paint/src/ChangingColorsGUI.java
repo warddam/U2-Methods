7777a24f9c26db345bbd8bed233be4730607f273
warddam001 <warddam001@warren.k12.in.us>
2021-10-22 12:42:21 -0400

    colors

--- L7-Swing Colors and Paint/src/ChangingColorsGUI.java
+++ L7-Swing Colors and Paint/src/ChangingColorsGUI.java
@@ -1,15 +1,57 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ChangingColorsGUI {
 
+
+
+        JFrame window;
+        JFrame panel;
+        JFrame colorClicker;
+
+
+        public static void main(String[] args) {
+
+
+
+            new ChangingColorsGUI();
+
+            window =  new JFrame("Color Changer");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setSize(350,137);
+            window.setLocationRelativeTo(null);
+
+            panel = new JPanel();
+            Colorclicker = new JButton("Change the colors");
+
+
+
+            Color panelBack = new Color(83, 75, 189, 155);
+            Color buttonBack = new Color(75, 213, 175);
+            Color buttonFront = new Color(100, 50, 27);
+
+
+
+
+
+
+            panel.setBackground(panelBack);
+            colorClicker.setForeground(buttonBack);
+            colorClicker.setForeground(buttonFront);
+
+
+
+
+        }
+
+
+    }
+
     //Variables in a class are declared here,
     //but often not initialized
 
-    JFrame window;
-    JPanel panel;
-    JButton colorClicker;
 
     /*
     This is a constructor and absolutely necessary for a class.
@@ -17,41 +59,7 @@ public class ChangingColorsGUI {
     name as the class.
     */
 
-    public static void main(String[] args) {
 
-    }
-
-    public ChangingColorsGUI() {
-
-        window = new JFrame("Color Changer");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(340, 127);
-        window.setLocationRelativeTo(null);
-
-        panel = new JPanel();
-        colorClicker = new JButton("Change the Colors");
-
-        //create a new Custom color
-        Color panelBack = new Color(220, 220, 220);
-        Color buttonBack = new Color(34, 49, 2);
-        Color buttonFront = new Color(100, 50, 27);
-
-        //Set the colors of our buttons and panel
-
-        panel.setBackground(panelBack);
-        colorClicker.setForeground(buttonBack);
-        colorClicker.setForeground(buttonFront);
-
-
-
-
-        panel.add(colorClicker);
-        window.add(panel);
-
-        window.setVisible(true);
-
-
-    }
 }
 
     //Add a listener to change the color when the button is clicked
