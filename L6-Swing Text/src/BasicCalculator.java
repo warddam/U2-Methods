56b8916c9578e02cdd86aaf83991cfc69c7745b4
DJ Ward <warddam001@warren.k12.in.us>
2021-11-01 06:11:56 -0400

    U2-Methods: FeetToMeters

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BasicCalculator {
+
 
     /* Create a calculator program with two text fields
     and 4 buttons, one for each of the arithmetic operations.
@@ -11,27 +13,70 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
+    public static JTextField inputNumber2 = new JTextField("");
+    public static JTextField inputNumber = new JTextField("");
+    public static JLabel finalLabel = new JLabel("");
 
     public static void main(String[] args) {
-JFrame window = new JFrame("Simple calculator");
-JPanel panel = new JPanel();
+        JFrame tab = new JFrame("Cal");
+        JPanel panel = new JPanel();
+        JLabel numberLabel = new JLabel("Number input: ");
+        JLabel numberLabel2 = new JLabel("Number input 1");
 
 
+        JButton xButt = new JButton("x");
+        JButton subButt = new JButton("-");
+        JButton addButt = new JButton("+");
+        JButton divButt = new JButton("/");
 
 
+        panel.add(xButt);
+        panel.add(divButt);
+        panel.add(subButt);
+        panel.add(addButt);
 
 
+        panel.add(inputNumber);
+
+        tab.add(panel);
+        tab.setSize(200,400);
+        tab.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tab.setLocationRelativeTo(null);
+
+        panel.add(inputNumber);
+        panel.add(numberLabel);
+        panel.add(inputNumber2);
+        panel.add(numberLabel2);
 
 
+        numberLabel.setSize(300, 20);
+        tab.setSize(1600, 1900);
 
+        tab.setVisible(true);
 
     }
+
+    private  static class addListener implements ActionListener{
+        public void actionPerformed(ActionEvent actionEvent){
+            double num1 =  Double.parseDouble(inputNumber.getText());
+            double num2 =  Double.parseDouble(inputNumber2.getText());
+
+            finalLabel.setText(String.valueOf(num1 + num2));
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
--- L7-Swing Colors and Paint/src/ColorSwitchGUI.java
+++ L7-Swing Colors and Paint/src/ColorSwitchGUI.java
@@ -1,11 +1,87 @@
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ColorSwitchGUI {
+    JFrame window = new JFrame();
+    JPanel panel = new JPanel();
+    JButton buttonRoyal = new JButton("Theme: Royal");
+    JButton buttonCreme = new JButton("Theme: Creme");
+    JButton buttonVanilla = new JButton("Theme: Vanilla");
 
     //Declare your frame, panel and three buttons.
     public ColorSwitchGUI(){
         //create your GUI
+
+        window = new JFrame("Color Switcher");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(340,125);
+        window.setLocationRelativeTo(null);
+
+        //Add Action Listeners to buttons
+        buttonRoyal.addActionListener( new Royal());
+        buttonCreme.addActionListener( new Creme());
+        buttonVanilla.addActionListener( new Vanilla());
+
+        panel.add(buttonRoyal);
+        panel.add(buttonCreme);
+        panel.add(buttonVanilla);
+
+        window.add(panel);
+        window.setVisible(true);
     }
 
+
     //add listeners to change the colors. Don't forget to link them to buttons.
+    private class Royal implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent){
+            Color panelBack = new Color(217, 140, 0);
+            Color buttonBack = new Color(15, 0, 255);
+            Color buttonText = new Color(255, 164, 0);
+
+            panel.setBackground(buttonBack);
+            buttonRoyal.setForeground(panelBack);
+            buttonRoyal.setBackground(buttonText);
+            buttonCreme.setForeground(panelBack);
+            buttonCreme.setBackground(buttonText);
+            buttonVanilla.setForeground(panelBack);
+            buttonVanilla.setBackground(buttonText);
+        }
+    }
+
+    private class Creme implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent){
+            Color panelBack = new Color(255, 130, 67);
+            Color buttonBack = new Color(253, 166, 93);
+            Color buttonText = new Color(255, 208, 127);
+
+            panel.setBackground(buttonBack);
+            buttonRoyal.setForeground(panelBack);
+            buttonRoyal.setBackground(buttonText);
+            buttonCreme.setForeground(panelBack);
+            buttonCreme.setBackground(buttonText);
+            buttonVanilla.setForeground(panelBack);
+            buttonVanilla.setBackground(buttonText);
+        }
+    }
+    private class Vanilla implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent){
+            Color panelBack = new Color(245, 198, 165);
+            Color buttonBack = new Color (255, 119, 119);
+            Color buttonText = new Color (162, 65, 107);
+
+            panel.setBackground(buttonBack);
+            buttonRoyal.setForeground(panelBack);
+            buttonRoyal.setBackground(buttonText);
+            buttonCreme.setForeground(panelBack);
+            buttonCreme.setBackground(buttonText);
+            buttonVanilla.setForeground(panelBack);
+            buttonVanilla.setBackground(buttonText);
+        }
+    }
 }
+
