d8de8f0e1f94afff09d827ba9921b3edf8b6f8cb
DJ Ward <warddam001@warren.k12.in.us>
2021-11-01 06:17:39 -0400

    U2-Methods: FeetToMeters

--- L7-Swing Colors and Paint/src/ColorSwitch.java
+++ L7-Swing Colors and Paint/src/ColorSwitch.java
@@ -5,26 +5,10 @@ the color of the panel background and the button backgrounds and foregrounds.
 Create the GUI in the ColorSwitchGUI class.
  */
 
-import javax.swing.*;
-
 public class ColorSwitch {
-JFrame frame;
-
-
-
-
-
-
-
-
 
     public static void main(String[] args) {
         //Create your class object
-
-
-
-
-
-
+        new ColorSwitchGUI();
     }
 }
